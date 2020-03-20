2a0dea3f6477b60a287b4627dfd09045ea5a4539
unknown <jethanhargrave@gmail.com>
2020-03-20 13:56:38 -0400

    MVP

--- starwars/src/components/PhotoCard.jsx
+++ starwars/src/components/PhotoCard.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import styled from 'styled-components'
+
+const StyleDiv = styled.div`
+width: 25%;
+color: white;
+background-color: rgba(255, 153, 138, 0.5);
+text-align: center;
+display: flex;
+flex-direction: column;
+padding: 2%;
+margin-left: 35%;
+border-radius: 20px;
+margin-bottom: 1%;
+`;
 
 const PhotoCard = (props) => {
     return (
-        <div>
+        <StyleDiv>
             <h2>Name: {props.name}</h2>
             <p>Birth Year: {props.birth_year}</p>
-        </div>
+        </StyleDiv>
     )
 }
 
